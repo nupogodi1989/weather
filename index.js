7ad9daf5bb862979f940b3a6123d3b85a00418d8
boris <basarkis@alumni.unca.edu>
2020-04-28 13:32:18 -0400

    setup weatherClientOptions with hosname and path format location input from console

--- index.js
+++ index.js
@@ -3,10 +3,30 @@
 'use strict'
 
 const process = require('process')
+const querystring = require('querystring')
+
+const apiKey = process.env.API_KEY
 
 const weatherLocations = process.argv.slice(2)
-try {
-  process.stdout.write(weatherLocations.toString())
-} catch (err) {
-  process.stderr.write(err.message)
+const weatherString = weatherLocations.join(' ')
+const weatherArray = weatherString.split(',')
+
+const weatherClientOptions = location => ({
+  hostname: 'api.openweathermap.org',
+  path: `/data/2.5/weather?q=${location}&appid=${apiKey}`,
+})
+
+const outputWeatherToUser = rawLocation => {
+  const trimmedLocation = rawLocation.trim()
+  const location = querystring.escape(trimmedLocation)
+  const options = weatherClientOptions(location)
+  console.log(options)
+  return options
 }
+
+const outputWeather = weather => {
+  process.stdout.write(weather)
+}
+
+outputWeather(weatherString)
+outputWeatherToUser(weatherString)
