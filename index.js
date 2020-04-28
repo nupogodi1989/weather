7a390058398d135ebd6936d994c243df578d3452
boris <basarkis@alumni.unca.edu>
2020-04-28 13:12:26 -0400

    make sure output input to console works and npm install require

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+'use strict'
+
+const process = require('process')
+
+const weatherLocations = process.argv.slice(2)
+try {
+  process.stdout.write(weatherLocations.toString())
+} catch (err) {
+  process.stderr.write(err.message)
+}
